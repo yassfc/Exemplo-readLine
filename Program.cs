8041b10e1d519439749cb7141b92505729b95730
yassfc <141974926+yassfc@users.noreply.github.com>
2023-08-12 03:38:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+
+
+string r = "reprovado";
+int nota;
+
+
+
+Console.WriteLine("Digite a nota");
+nota = Convert.ToInt32 (Console.ReadLine());
+
+
+if(nota >= 50){
+
+r = "aprovado";
+
+    
+}
+
+Console.WriteLine("Resultado: {0}", r);
+
+}
+     
+
+
+        }
+    
+
+
+
+
+
+
+    
